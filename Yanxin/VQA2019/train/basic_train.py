8c8d982f129266e249ea9fb49a5a4f3ef9539f41
yanxinzju <827358764@qq.com>
2019-03-23 16:01:46 +0800

    Update basic_train.py

--- Yanxin/VQA2019/train/basic_train.py
+++ Yanxin/VQA2019/train/basic_train.py
@@ -16,7 +16,8 @@ val_vgg_feature = tables.open_file('../file/val_vgg16_feature.h5').root.vgg16
 #     train_data=med_data()
 
 
-def Modality_train():
+
+def Modality_train(pretrained=False):
     train_data=med_data('Modality.csv',mode='train')
     train_loader=DataLoader(dataset=train_data,batch_size=config.batch_size,shuffle=True,drop_last=True)
 
@@ -27,7 +28,15 @@ def Modality_train():
 
     num_class = len(answer_set)
 
-    model=Modality(vocab2int,embedding,num_class).to(config.device)
+    model = Modality(vocab2int, embedding, num_class).to(config.device)
+
+    if pretrained==False:
+        ckpt_id=0
+    else:
+        ckpt_id=load_last_ckpt('Modality')
+        print('load pretrained model at ckpt_{}'.format(ckpt_id))
+        model.load_state_dict(torch.load('../log/Modality/' + 'ckpt_' + str(ckpt_id) + '.pkl'))
+        ckpt_id+=1
 
     criterion=nn.CrossEntropyLoss()
 
@@ -60,24 +69,29 @@ def Modality_train():
         total_loss=total_loss/j
         total_acc=total_acc/train_data.__len__()
         print('\n')
-        print('train epoch={}/{},train loss=: {},train acc=: {}'.format(epoch,config.epoch,total_loss,total_acc))
+        print('train epoch={}/{},train loss=: {},train acc=: {}'.format(epoch+ckpt_id,config.epoch+ckpt_id,total_loss,total_acc))
         print('\n')
-        torch.save(model,'../log/Modality/'+'ckpt_'+str(epoch)+'.pth')
-        Modality_val(epoch)
+        torch.save(model.state_dict(),'../log/Modality/'+'ckpt_'+str(epoch+ckpt_id)+'.pkl')
+        Modality_val(epoch+ckpt_id)
 
 
 def Modality_val(ckpt_id):
     val_data=med_data('Modality.csv',mode='val')
     val_loader=DataLoader(dataset=val_data,batch_size=config.val_size,shuffle=False,drop_last=True)
 
+    vocab2int=load_json('Modality','vocab2int')
+    embedding=np.load('../file/Modality/embedding.npy')
     name2id=load_name2id(mode='val')
     answer_set=load_answer_set('Modality')
 
+    num_class = len(answer_set)
+
+    model = Modality(vocab2int, embedding, num_class).to(config.device)
 
     criterion = nn.CrossEntropyLoss()
 
-    print('load model ckpt_{}'.format(ckpt_id))
-    model=torch.load('../log/Modality/'+'ckpt_'+str(ckpt_id)+'.pth').to(config.device)
+    print('load model at ckpt_{}'.format(ckpt_id))
+    model.load_state_dict(torch.load('../log/Modality/' + 'ckpt_' + str(ckpt_id) + '.pkl'))
 
     total_loss=0
     total_acc=0
@@ -92,15 +106,18 @@ def Modality_val(ckpt_id):
         out=model(question,image_feature)
         loss=criterion(out,answer)
         predict=torch.argmax(out,1)
+        # print(predict)
+        # print(answer)
+        # print('\n')
         acc=torch.sum(predict==answer)
         total_acc+=acc.item()
         total_loss+=loss.item()
     total_loss=total_loss/val_data.__len__()
     total_acc=total_acc/val_data.__len__()
-    print('train loss=: {},train acc=: {}'.format(total_loss,total_acc))
+    print('val loss=: {},val acc=: {}'.format(total_loss,total_acc))
     print('\n')
     print('\n')
 
 if __name__ == '__main__':
-    # Modality_train()
-    Modality_val(19)
+    Modality_train(pretrained=True)
+    # Modality_val(19)

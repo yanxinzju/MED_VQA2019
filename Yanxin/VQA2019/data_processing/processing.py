0876baeedc6a13670771646549590e67bef7b7ce
yanxinzju <827358764@qq.com>
2019-03-23 14:34:21 +0800

    Update processing.py

--- Yanxin/VQA2019/data_processing/processing.py
+++ Yanxin/VQA2019/data_processing/processing.py
@@ -7,5 +7,21 @@ def gen_all_embedding():
     gen_embedding('Modality',glove)
     gen_embedding('Organ',glove)
     gen_embedding('Plane',glove)
+
+def gen_all_answer_set():
+    gen_answer_set('Modality')
+    gen_answer_set('Abnormality')
+    gen_answer_set('Organ')
+    gen_answer_set('Plane')
+
 if __name__ == '__main__':
-    gen_all_embedding()
+    # gen_all_embedding()
+    # gen_all_answer_set()
+    answer_set1=load_answer_set('Modality')
+    answer_set2=load_answer_set('Abnormality')
+    answer_set3=load_answer_set('Organ')
+    answer_set4=load_answer_set('Plane')
+    # print(answer_set1)
+    # print(answer_set2)
+    # print(answer_set3)
+    # print(answer_set4)
